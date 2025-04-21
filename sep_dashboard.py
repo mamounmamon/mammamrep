fe883ce863a836738991a6f31c5bfb75bebdfb4e
mamounmamon <hattab.mamoun@gmail.com>
2025-04-21 20:55:55 +0100

    Update sep_dashboard.py

--- sep_dashboard.py
+++ sep_dashboard.py
@@ -6,7 +6,7 @@ import random
 import time
 import datetime
 #from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 
 st.set_page_config(layout="wide", page_title="Advanced ICU Sepsis & Condition Dashboard")
 
