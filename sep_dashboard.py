14331a5234ba928a2ed4d71173b19011e45965a3
mamounmamon <hattab.mamoun@gmail.com>
2025-04-21 12:06:08 +0100

    Update sep_dashboard.py

--- sep_dashboard.py
+++ sep_dashboard.py
@@ -147,7 +147,7 @@ with left:
 
     col1, col2, col3 = st.columns(3)
     col1.metric("HR", f"{vitals['HR']} bpm")
-    col2.metric("BP", f"{vitals['BP']}")
+    col2.metric("BP", f"{vitals['BP']} X")
     col3.metric("Temp", f"{vitals['Temp'][-1]} °C")
     col1.metric("SpO₂", f"{vitals['SpO2'][-1]}%")
     col2.metric("RR", f"{vitals['RR'][-1]}")
