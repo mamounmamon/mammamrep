bbd72fb93631a8de6947058e01334d86e0872905
mamounmamon <hattab.mamoun@gmail.com>
2025-04-26 16:33:10 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 import datetime
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 st.set_page_config(layout="wide", page_title="Advanced ICU Sepsis & Condition Dashboard")
